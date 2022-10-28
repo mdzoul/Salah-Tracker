7839cc0484e134cee971ac3e5a4b24b9c1646e4e
Zoul Aimi <82052598+mdzoul@users.noreply.github.com>
2022-10-28 17:52:36 +0800

    Tidied up code

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ import os
 from datetime import datetime
 import requests
 
-QUANTITY = input("How many times did you perform Salah today?\n")
+START = input("""
+Would you like to:
+1 - Post a pixel
+2 - Update a pixel
+3 - Delete a pixel
+""")
 
 TODAY = datetime.now().strftime("%Y%m%d")
 USERNAME = "zoul"
@@ -44,38 +49,52 @@ GRAPH_CONFIG = {
 # print(graph_response.text)
 
 # ------------------- DELETE GRAPH ------------------- #
-# delete_graph_response = requests.delete(url=f"{GRAPH_ENDPOINT}/{GRAPH_ID}", json=GRAPH_CONFIG, headers=HEADERS)
+# delete_graph_response = requests.delete(
+#     url=f"{GRAPH_ENDPOINT}/{GRAPH_ID}",
+#     json=GRAPH_CONFIG,
+#     headers=HEADERS
+# )
 # print(delete_graph_response.text)
 
 # ------------------- POST A PIXEL ------------------- #
-POST_PIXEL_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/{GRAPH_ID}"
-POST_PIXEL_CONFIG = {
-    "date": TODAY,
-    "quantity": QUANTITY,
-}
+if START == "1":
+    POST_PIXEL_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/{GRAPH_ID}"
 
-post_pixel_response = requests.post(
-    url=POST_PIXEL_ENDPOINT,
-    json=POST_PIXEL_CONFIG,
-    headers=HEADERS
-)
-print(post_pixel_response.text)
+    quantity = input("How many times did you perform Salah today?\n")
+
+    POST_PIXEL_CONFIG = {
+        "date": TODAY,
+        "quantity": quantity,
+    }
+
+    post_pixel_response = requests.post(
+        url=POST_PIXEL_ENDPOINT,
+        json=POST_PIXEL_CONFIG,
+        headers=HEADERS
+    )
+    print(post_pixel_response.text)
 
 # ------------------- UPDATE A PIXEL ------------------- #
-UPDATE_PIXEL_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/{GRAPH_ID}/<yyyyMMdd>"
-UPDATE_PIXEL_CONFIG = {
-    "quantity": QUANTITY,
-}
+if START == "2":
+    UPDATE_PIXEL_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/{GRAPH_ID}/{TODAY}"
 
-# update_pixel_response = requests.put(
-#     url=UPDATE_PIXEL_ENDPOINT,
-#     json=UPDATE_PIXEL_CONFIG,
-#     headers=HEADERS
-# )
-# print(update_pixel_response.text)
+    quantity = input("How many times did you perform Salah today?\n")
+
+    UPDATE_PIXEL_CONFIG = {
+        "quantity": quantity,
+    }
+
+    update_pixel_response = requests.put(
+        url=UPDATE_PIXEL_ENDPOINT,
+        json=UPDATE_PIXEL_CONFIG,
+        headers=HEADERS
+    )
+    print(update_pixel_response.text)
 
 # ------------------- DELETE A PIXEL ------------------- #
-DELETE_PIXEL_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/{GRAPH_ID}/<yyyyMMdd>"
+if START == "3":
+    delete_date = input("Enter date to delete:\n(yyyyMMdd)\n")
+    DELETE_PIXEL_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/{GRAPH_ID}/{delete_date}"
 
-# delete_pixel_response = requests.delete(url=DELETE_PIXEL_ENDPOINT, headers=HEADERS)
-# print(delete_pixel_response.text)
+    delete_pixel_response = requests.delete(url=DELETE_PIXEL_ENDPOINT, headers=HEADERS)
+    print(delete_pixel_response.text)
